c764869ffaa8cfeaaf94fec6f448573688f1e09f
CptMcBiggins <bricedickinson54@gmail.com>
2023-08-07 20:39:40 -0400

    Elastic Sync Update

--- development/update_alert.py
+++ development/update_alert.py
@@ -6,14 +6,16 @@ api_key = os.environ['ELASTIC_KEY']
 url = 'https://detectionengineering101-1f562a.kb.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules'
 headers = {'Authorization': 'ApiKey '+api_key, 'kbn-xsrf': 'true','Content-Type': 'application/json'}
 
+changed_files = os.environ["CHANGED_FILES"]
+
 for root, dirs, files in os.walk("detections/"):
 	for file in files:
-		data = "{\n"
-		if file.endswith('.toml'):
-			full_path = os.path.join(root, file)
-			with open(full_path, "rb") as toml:
-				alert = tomllib.load(toml)
-
+		if file in changed_files:
+			data = "{\n"
+			if file.endswith('.toml'):
+				full_path = os.path.join(root, file)
+				with open(full_path, "rb") as toml:
+					alert = tomllib.load(toml)
 				if alert['rule']['type'] == 'query':
 					required_fields = ['author','description','name','rule_id','risk_score','severity','type','query','threat']
 				elif alert['rule']['type'] == 'eql':
@@ -41,7 +43,7 @@ for root, dirs, files in os.walk("detections/"):
 							data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
 				data += "  \"enabled\": true\n}"
 
-		rule_id = alert['rule']['rule_id']
-		url = url + "?rule_id=" + rule_id
-		elastic_data = requests.put(url, headers=headers, data=data).json()
-		print(elastic_data)
+			rule_id = alert['rule']['rule_id']
+			url = url + "?rule_id=" + rule_id
+			elastic_data = requests.put(url, headers=headers, data=data).json()
+			print(elastic_data)
